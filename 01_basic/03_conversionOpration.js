b4110ccf7f3bc9e3db6920a745182fcce6b7dbb2
Prashant Bajak <bajakprashant1798@gmail.com>
2023-09-15 09:59:14 +0000

    datatype conversion opration

---
+++ 01_basic/03_conversionOpration.js
@@ -0,0 +1,31 @@
+let score = undefined;
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"55" => 55
+"33abc" => NaN
+true => 1
+null => 0
+undefined => NaN
+*/
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+/**
+ 1 => true
+ 0 => false
+ "" => false
+ bajak => true
+*/
+
+
+let someNumber = 25;
+let stringSomeNumber = String(someNumber)
+console.log(typeof stringSomeNumber);
+console.log(stringSomeNumber);
