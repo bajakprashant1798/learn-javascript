d7df1abcfd921f4b12b8cd3730b89790c6d7740b
Prashant Bajak <bajakprashant1798@gmail.com>
2023-09-15 09:37:50 +0000

    datatypes

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,3 +1,20 @@
 "use strict"; // treat all JS code as newer version
 
 // tc39 and mdn for documentation
+
+// datatypes
+/* ** premative datatypes
+    * number
+    * BigInt
+    * string
+    * boolean
+    * null => standalone value
+    * undefined => value is not defined
+    * symbol => unique
+*/
+/* ** non premative datatypes **
+    * object
+*/
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // object
