902605427618aaf17cd4db3ea6446620bfafe86c
Prashant Bajak <bajakprashant1798@gmail.com>
2023-09-16 17:28:57 +0000

    nums and maths

--- 01_basic/04_comparision.js
+++ 01_basic/04_comparision.js
@@ -18,3 +18,16 @@
 
 
 // **  "==="  compares variables and with it's datatypes too
+
+const myName = String('Hitesh-hc')
+
+// console.log(myName.substring(8,3));
+// console.log(myName.slice(-8,4));
+// const myNameSlice = myName.slice(8,3)
+// console.log(myNameSlice.length); //output will be blank
+
+const emptyName = String("")
+
+console.log(typeof emptyName);
+console.log(emptyName);
+console.log(emptyName.length);
