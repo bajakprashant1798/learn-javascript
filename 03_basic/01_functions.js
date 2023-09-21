681c5db2b07d2de662dc794b1bd501abcc0556b1
Prashant Bajak <bajakprashant1798@gmail.com>
2023-09-21 15:15:14 +0000

    functions_02

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -33,16 +33,54 @@ const result = addTwoNumber(2,3)
 // console.log("Result:", result);    // undefined
 
 
-// // here we can see example of string interpolation too
+//// (string interpolation) here we can see example of string interpolation too
 // function loginUserMessage(username) {
 //     return `${username} just logged in`
 // }
 // console.log(loginUserMessage("Prashant"));
 // console.log(loginUserMessage());    // result would be undefined just logged in
 
-// default username is ram
+//// default username is ram
 function loginUserMessage(username = "ram") {
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Prashant"));  // this will override username ram into Prashant
-console.log(loginUserMessage());
+// console.log(loginUserMessage("Prashant"));  // this will override username ram into Prashant
+// console.log(loginUserMessage());
+
+
+// // rest operator
+// function calculateCartPrice(...number1) {
+//     return number1
+// }
+// console.log(calculateCartPrice(200, 300, 500));
+
+function calculateCartPrice(val1, val2, ...number1) {
+    return number1
+}
+console.log(calculateCartPrice(200, 300, 500, 2000));
+
+const user = {
+    username: "Prashant",
+    price: 562
+}
+
+// Object and function
+// (string interpolation)
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user)
+//// another way
+handleObject({
+    username: "ram",
+    price: 698
+})
+
+// Array and function
+const myNewArray = [200, 500, 300, 800]
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+// // anotherway
+console.log(returnSecondValue([200, 500, 300, 800]));
