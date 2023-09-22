a36884b05c737f87511759107199a06a64220db0
Prashant Bajak <bajakprashant1798@gmail.com>
2023-09-22 18:41:51 +0000

    filter map

---
+++ 05_iterations/06_map_reduce.js
@@ -0,0 +1,26 @@
+const coding = ["js", "python", "java", "ruby", "cpp"]
+
+// const values = coding.forEach((item) => {
+//     console.log(item);
+//     return item  // won't return anything. foreach does not return anything
+// });
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+////filter return values
+// const newNums = myNums.filter((nums) => nums > 4)
+// const newNums2 = myNums.filter((num) => {
+//     return num > 4
+// })
+// console.log(newNums);
+// console.log(newNums2);
+
+// const newNums3 = []
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums3.push(num)
+//     }
+// });
+// console.log(newNums3);
+
